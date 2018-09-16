29ded6926c136371af864e858a4c42ac50799505
Alan Rocha <alanrocha.web@outlook.com>
2018-09-15 23:02:45 -0300

    react-router e roteamento das páginas da Home e About

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Home from './pages/Home';
+import About from './pages/About';
 
 const App = props => {
   return (
     <div>
-      <Home />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
